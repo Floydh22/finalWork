c3f75b53686da30b4acd16cdb4f8cb9edd781451
Floyd <fnhaslett22@gmail.com>
2022-04-24 21:05:26 -0600

    Fixed Database post and get

--- final-website/src/Api/routes/users.js
+++ final-website/src/Api/routes/users.js
@@ -4,30 +4,48 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var db = req.app.locals.db;
-  db.collection("users").find({}, {projection: {_id:0, color: 1}}).toArray(function(err,result){
-    if(err) throw err;
-    res.json(result);
-  });
 
+  db.collection("users")
+  .findOne()
+  .then(result => {
+    console.log(result);
+
+    res.send(result.color);
+  });
 });
+
 
 router.get('/:color', function(req, res, next) {
-  var color = req.params.color;
-  for(let light of lights){
-    if(light.color == color){
-      res.json(light.color);
-    }
-  }
-  res.send("Cannot find color");
-});
-
-router.post("/", function(req, res, next){
-  const color = {
-    "color": req.body.color
-  }
 
   var db = req.app.locals.db;
-  db.collection("users").insertOne(color);
+  var color = req.params.color;
+
+  db.collection("users").find({}, {projection: {_id:0, color: 1}}).toArray(function(err,results){
+    if(err) throw err;
+    for(let result of results){
+      if (result.color == color){
+        res.send(result.color)
+      }
+    res.send("Cannot find color");
+    }
+  })
+});
+
+
+router.post("/", function(req, res, next){
+  const color = req.body.color;
+
+  var db = req.app.locals.db;
+  db.collection("users").updateOne(
+    {"current":"true"},
+    {
+      $set:{
+        "color" : color
+      }
+      
+    },
+      {upsert:true}
+  );
   res.send("Color inserted");
 });
 
